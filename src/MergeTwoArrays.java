559beea018cb083f69af5c223b55b981b083e22a
SAIKIRANVAKADA <116869780+SAIKIRANVAKADA@users.noreply.github.com>
2024-10-17 18:20:40 +0530

    completed on 17-10-24

---
+++ src/MergeTwoArrays.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class MergeTwoArrays {
+	
+	static char[]  mergeData(ArrayList<Character> arrayList1,ArrayList<Character> arrayList2) {
+		char[] outPutArray = new char[arrayList1.size()+ arrayList2.size()];
+		arrayList1.addAll(arrayList2);
+//		System.out.println(arrayList1);
+		
+		for(char i=0 ; i<arrayList1.size();i++) {
+			outPutArray[i]=arrayList1.get(i);
+		}
+		
+		return
+				outPutArray;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			ArrayList<Character> arr1= new ArrayList<Character>();
+			arr1.add('a');
+			arr1.add('b');
+			arr1.add('c');
+			arr1.add('d');
+			ArrayList<Character> arr2= new ArrayList<Character>();
+			arr2.add('e');
+			arr2.add('f');
+			arr2.add('g');
+			arr2.add('h');
+			char[] myArray = mergeData(arr1, arr2);
+			for(char c: myArray) {
+			System.out.println(c );}
+	}
+
+}
