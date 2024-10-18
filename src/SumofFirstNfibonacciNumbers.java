3f5dea412698dff6faca71d0b2d6eeb3cbbfb9a1
SAIKIRANVAKADA <116869780+SAIKIRANVAKADA@users.noreply.github.com>
2024-10-18 14:58:07 +0530

    completed on 18-10-24

---
+++ src/SumofFirstNfibonacciNumbers.java
@@ -0,0 +1,37 @@
+public class SumofFirstNfibonacciNumbers {
+	
+	static int factof(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		return n * factof(n - 1);
+	}
+	
+	static int fibonacci(int n) {
+		if(n<=1) {
+			return n;
+		}
+		else {
+			  return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+	
+	static int sumOfFactorial(int num) {
+		int sum = 0;
+		for(int i = 0;i<=num;i++) {
+			
+			int fib = fibonacci(i);  
+            sum += factof(fib); 
+			
+		}
+	return sum-1;
+		}
+		
+		
+		
+
+	public static void main(String[] args) {
+		int n=5;
+	System.out.println(sumOfFactorial(n));
+	}
+}
