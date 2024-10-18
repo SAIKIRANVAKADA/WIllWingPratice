6c88397f038afa3112726fe3acc71bb88714f723
SAIKIRANVAKADA <116869780+SAIKIRANVAKADA@users.noreply.github.com>
2024-10-18 14:52:29 +0530

    SumOfNaturalNumbersDivisibleBy3And8.java

--- src/SumOfNaturalNumbersDivisibleBy3And8.java
+++ src/SumOfNaturalNumbersDivisibleBy3And8.java
@@ -3,7 +3,7 @@ public class SumOfNaturalNumbersDivisibleBy3And8 {
 	static int calculateSum(int n) {
 		int sum =0 ;
 		while(n >0 ){
-			if(n%2==0 &&(n%3==0 || n%8==0)) {
+			if(n%2==0 &&(n%3==0 && n%8==0)) {
 				sum = sum + n;
 			}
 			n--;
