05f42e2bbf828be7da381c02532c5d492656ce11
SAIKIRANVAKADA <116869780+SAIKIRANVAKADA@users.noreply.github.com>
2024-10-18 18:19:25 +0530

    completed on 18-10-24

---
+++ src/changeString.java
@@ -0,0 +1,86 @@
+
+import java.util.LinkedHashSet;
+
+public class ModifyStringBasedonUserChoice {
+
+
+
+
+	static String changeString(String inputString,char inputChar) {
+		
+		String newStr= Character.toString(inputChar).toUpperCase();
+		
+		StringBuffer mybuffer = new StringBuffer(inputString);
+		
+		switch(newStr) {
+		
+		case "A":{
+			mybuffer.append(inputString);
+			break;
+			}
+		
+		
+		
+		case "B":
+			
+			for(int i = 0 ;i<inputString.length();i++) {
+				
+				if(i%2==0) {
+				mybuffer.setCharAt(i, '*');
+					
+				}
+				
+			}break;
+			
+			
+		case "C":
+		
+			LinkedHashSet<Character> myLinkedhashset = new LinkedHashSet<Character>();
+			
+			
+			for(int j= 0;j<mybuffer.length();j++) {
+				myLinkedhashset.add(mybuffer.charAt(j));
+			}
+			mybuffer.delete(0, mybuffer.length());
+			for(char c: myLinkedhashset) {
+				String s = Character.toString(c);
+				
+				mybuffer.append(s);
+				
+			}
+			break;
+
+		
+			
+			
+			
+		
+		case "D":
+			
+			 for (int p = 0; p < inputString.length(); p++) {
+				 if(p%2==0) {
+                 mybuffer.setCharAt(p,(char) (inputString.charAt(p) - 32)); // Convert to lowercase
+				 }
+				 
+             }
+			break;
+			
+			
+			
+			
+			
+			
+			
+		} //switchend
+		return mybuffer.toString();
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(changeString("saikiran", 'B'));
+
+	}
+
+}
